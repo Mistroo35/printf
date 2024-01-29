dc321f7b05a3192510d7f5374bbd6c3bfdefc316
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-01-29 07:29:46 +0200

    string function

---
+++ printString.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * printString - print a string.
+ * @String: string to print.
+ * Return: the length of the String.
+ */
+
+int printString(const char *String)
+{
+	int i, len;
+
+	len = strlen(String);
+	if (String != NULL)
+	{
+		for (i = 0; i < len; i++)
+		{
+			putchar(String[i]);
+		}
+	}
+	else
+	{
+		const char *nill = "nill";
+		len = strlen(nill);
+		for (i = 0; i < len; i++)
+		{
+			putchar(nill[i]);
+		}
+	}
+	return (len);
+}
