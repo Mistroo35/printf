7ec4e023978eba37e450aefc7b00ed7447f03e6e
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-01-31 02:30:54 +0200

    Add new loines

--- .ccls-cache/@home@mohammed@Documents@printf/main.h
+++ .ccls-cache/@home@mohammed@Documents@printf/main.h
@@ -2,9 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
-#include <limits.h>
 #include <unistd.h>
 
 
@@ -47,3 +45,4 @@ int _putchar(char c);
 int _printf(const char *Format, ...);
 
 #endif
+
--- main.h
+++ main.h
@@ -2,9 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
-#include <limits.h>
 #include <unistd.h>
 
 
