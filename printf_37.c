a9997a8950b5241697fb4d4c42fce7f66944650d
Mistroo35 <musasaifaldeen35@gmail.com>
2024-02-01 07:24:06 -0500

    commit

--- printf_37.c
+++ printf_37.c
@@ -1,8 +1,10 @@
 #include "main.h"
 /**
- * printf_37 - prints the char 37.
+ * printf_37 -function that prints the char 37.
  * Return: 1.
  */
+
+//function declaration
 int printf_37(void)
 {
 	_putchar(37);
